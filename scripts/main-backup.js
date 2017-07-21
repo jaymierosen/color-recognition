79e786a43d22a00521deaf63e749928236f06983
Jaymie Rosen <rosen.jaymie@gmail.com>
2017-07-20 21:30:35 -0400

    working with sounds now

---
+++ scripts/main-backup.js
@@ -0,0 +1,107 @@
+//app
+const app = {};
+//important info
+app.appInfo = {
+	client_id: 'O-a-HYqJOWapnOf58H-SLT22JMx8FnEsDbUzquCt',
+	client_secret: 'Hfelwk7UUxr1SCWUnJePHnnVCn7jspKQzmBncom5',
+	api_key: 'cffd7ff0579747dfb2980d4437f91d6a',
+	model: 'eeed0b6733a644cea07cf4c60f87ebb7'
+};
+//colors array
+app.colors = [];
+// app.newColors;
+//shapes
+app.shapes = [
+	circle = {
+		x: 0,
+		y: 0,
+		w: 0,
+		h: 0,
+	},
+	rectangle = {
+		x: 0,
+		y: 0,
+		w: 0,
+		h: 0,
+	}
+]
+//making a clarifai app
+app.clarifaiApp = new Clarifai.App(app.appInfo.client_id, app.appInfo.client_secret);
+//setup function
+function setup(){
+	var canvas = createCanvas(640, 480);
+	canvas.position(0, 0);
+	noStroke();
+	colorMode(RGB, 50, 50, 50, 5);
+};
+//draw function
+function draw(){
+	app.getColors = function(info){
+		let colorData = info.data.outputs[0].data.colors;
+		app.colors = colorData.map(function(color) {
+			return app.hexToRGB(color.raw_hex);
+		});
+		for(var i in colorData){
+			console.log(i);
+			// app.colors.push(app.hexToRGB(colorData[i].raw_hex));
+			stroke(255, 255, 255);
+			// fill(app.colors[i].r, app.colors[i].g, app.colors[i].b);
+			fill(random(255), random(255), random(255));
+			app.shapes[0].x = app.shapes[0].x + 50;
+			app.shapes[0].y = app.shapes[0].y + 50;
+			app.shapes[0].w = app.shapes[0].w + 50;
+			app.shapes[0].h = app.shapes[0].h + 50;
+			// ellipse(app.shapes[0].x, app.shapes[0].y, app.shapes[0].w, app.shapes[0].h);
+			ellipse(mouseX, mouseY, 50, 50);
+			triangle(mouseX, mouseY, 58, 20, 86, 75);
+			// stroke(0, 0, 0);
+			// fill(app.shapes[1].r, app.shapes[1].g, app.shapes[1].b);
+			// app.shapes[1].x = app.shapes[1].x + 50;
+			// app.shapes[1].y = app.shapes[1].y + 50;
+			// app.shapes[1].w = app.shapes[1].w + 50;
+			// app.shapes[1].h = app.shapes[1].h + 50;
+			// rect(app.shapes[1].x, app.shapes[1].y, app.shapes[1].w, app.shapes[1].h);
+			// app.colors.forEach(function(color){
+			// 	app.newColors = [];
+			// 	app.newColors.push(color);
+			// });
+			console.log(app.colors[i]);
+		};
+	};
+}
+//running the api call
+app.clarifaiCheck = function(imageUrl){
+	app.clarifaiApp.models.predict(app.appInfo.model, imageUrl)
+		.then(function(res){
+			app.getColors(res);
+		});
+	};
+//converting hex to RGB or RGBA
+//thank you --> https://stackoverflow.com/questions/21646738/convert-hex-to-rgba
+app.hexToRGB = function(hex, alpha){
+    var r = parseInt(hex.slice(1, 3), 16);
+    var g = parseInt(hex.slice(3, 5), 16);
+    var b = parseInt(hex.slice(5, 7), 16);
+	if(alpha) {
+        return { r: r, g: g, b: b, a: alpha };
+    } else {
+        return { r: r, g: g, b: b };
+    }
+}
+//getting value from url input
+app.getVal = function(){
+	$('form#main__form').on('submit', function(e){
+		e.preventDefault();
+		let inputVal = $('input#url').val();
+		app.clarifaiCheck(inputVal);
+	});
+};
+//init function
+app.init = function(){
+	app.clarifaiCheck();
+	app.getVal();
+};
+// document ready
+$(function(){
+  app.init();
+});
--- scripts/main.js
+++ scripts/main.js
@@ -1,117 +1,48 @@
-//app
 const app = {};
-//important info
-app.appInfo = {
-	client_id: 'O-a-HYqJOWapnOf58H-SLT22JMx8FnEsDbUzquCt',
-	client_secret: 'Hfelwk7UUxr1SCWUnJePHnnVCn7jspKQzmBncom5',
-	api_key: 'cffd7ff0579747dfb2980d4437f91d6a',
-	model: 'eeed0b6733a644cea07cf4c60f87ebb7'
-};
-//colors array
-app.colors = [];
-// app.newColors;
-//shapes
-app.shapes = [
-	circle = {
-		x: 0,
-		y: 0,
-		w: 0,
-		h: 0,
-		// r: r,
-		// g: g,
-		// b: b,
-		r: 0,
-		g: 0,
-		b: 0,
-		a: 0
-	},
-	rectangle = {
-		x: 0,
-		y: 0,
-		w: 0,
-		h: 0,
-		r: 0,
-		g: 0,
-		b: 0,
-		a: 0
-	}
-]
-//making a clarifai app
-app.clarifaiApp = new Clarifai.App(app.appInfo.client_id, app.appInfo.client_secret);
+app.importantInfo = {
+	client_id: '55c06bc6b9bd4e68a348ea62efaab485',
+	client_secret: 'e8d12fcc701c447492ccbe15b2c80d75'
+}
+app.getArists = (artist) => $.ajax({
+	url: `https://api.spotify.com/v1/search?=${app.client_id}`,
+	method: 'GET',
+	dataType: 'json',
+
+});
+// app.getAristsAlbums = (id) => $.ajax({
+// 	url: `https://api.spotify.com/v1/artists/${id}/albums`,
+// 	method: 'GET',
+// 	dataType: 'json',
+// 	data: {
+// 		album_type: 'album',
+// 	}
+// });
+// app.getAlbumTracks = (id) => $.ajax({
+// 	url: `https://api.spotify.com/v1/albums/${id}/tracks`,
+// 	method: 'GET',
+// 	dataType: 'json'
+// });
+var mic;
 //setup function
 function setup(){
-	createCanvas(600, 600);
-	background(235, 235, 235);
+	var canvas = createCanvas(640, 480);
+	canvas.position(0, 0);
+	noStroke();
+	colorMode(HSB);
+	mic = new p5.AudioIn();
+	mic.start();
 };
 //draw function
 function draw(){
-	app.getColors = function(info){
-		let colorData = info.data.outputs[0].data.colors;
-
-		app.colors = colorData.map(function(color) {
-			return app.hexToRGB(color.raw_hex);
-		});
-
-
-		for(var i in colorData){
-			console.log(i);
-			// app.colors.push(app.hexToRGB(colorData[i].raw_hex));
-			stroke(255, 255, 255);
-			fill(app.colors[i].r, app.colors[i].g, app.colors[i].b);
-			app.shapes[0].x = app.shapes[0].x + 50;
-			app.shapes[0].y = app.shapes[0].y + 50;
-			app.shapes[0].w = app.shapes[0].w + 50;
-			app.shapes[0].h = app.shapes[0].h + 50;
-			ellipse(app.shapes[0].x, app.shapes[0].y, app.shapes[0].w, app.shapes[0].h);
-			// stroke(0, 0, 0);
-			// fill(app.shapes[1].r, app.shapes[1].g, app.shapes[1].b);
-			// app.shapes[1].x = app.shapes[1].x + 50;
-			// app.shapes[1].y = app.shapes[1].y + 50;
-			// app.shapes[1].w = app.shapes[1].w + 50;
-			// app.shapes[1].h = app.shapes[1].h + 50;
-			// rect(app.shapes[1].x, app.shapes[1].y, app.shapes[1].w, app.shapes[1].h);
-			// app.colors.forEach(function(color){
-			// 	app.newColors = [];
-			// 	app.newColors.push(color);
-			// });
-			console.log(app.colors[i]);
-		};
-
-	};
-}
-//running the api call
-app.clarifaiCheck = function(imageUrl){
-	app.clarifaiApp.models.predict(app.appInfo.model, imageUrl)
-		.then(function(res){
-			app.getColors(res);
-		});
-	};
-//converting hex to RGB or RGBA
-//thank you --> https://stackoverflow.com/questions/21646738/convert-hex-to-rgba
-app.hexToRGB = function(hex, alpha){
-    var r = parseInt(hex.slice(1, 3), 16);
-    var g = parseInt(hex.slice(3, 5), 16);
-    var b = parseInt(hex.slice(5, 7), 16);
-	if(alpha) {
-        return { r: r, g: g, b: b,  a: alpha };
-    } else {
-        return { r: r, g: g, b: b };
-    }
-}
-//getting value from url input
-app.getVal = function(){
-	$('form#main__form').on('submit', function(e){
-		e.preventDefault();
-		let inputVal = $('input#url').val();
-		app.clarifaiCheck(inputVal);
-	});
+	fill(random(255), 255, 255);
+	var d = map(mic.getLevel(), 0, 0.5, 1, 50);
+	var y = map(mic.getLevel(), 0, 0.5, height, 0);
+	ellipse(50, y, d, d);
 };
-//init function
 app.init = function(){
-	app.clarifaiCheck();
-	app.getVal();
-};
-// document ready
+	app.getArists();
+	console.log('hello, world?');
+}
 $(function(){
-  app.init();
+	app.init();
 });
