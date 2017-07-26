649f632aa596d577e8e90ff8c2aee10362a7f7f6
Jaymie Rosen <rosen.jaymie@gmail.com>
2017-07-25 21:35:29 -0400

    whoooooo working

--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,9 @@ let mic;
 let canvas;
 let sound;
 let soundGroup;
+app.sound = null;
+let value = 0;
+let pos = 25;
 //setup function
 function setup(){
 	//setting up the canvas
@@ -19,19 +22,36 @@ function setup(){
 //draw function
 function draw(){
 	fill(random(255), random(255), random(255));
-	const diameter = map(mic.getLevel(), 0, 0.9, 1, 1600);
-	ellipse(width/2, height/2, diameter, diameter);
+	const x = map(mic.getLevel(), 0, 0.9, 1, 1600);
+	const y = map(mic.getLevel(), 0, 0.9, 1, 1600);
+	ellipse(width / (Math.random() * 5) , height / (Math.random() * 5 ), x, y);
+	triangle(width / (Math.random() * 5) , height / (Math.random() * 5 ), x, y);
+	setShakeThreshold(30);
 };
-app.init = function() {
+function deviceShaken() {
+	value = value + 5;
+	if (value > 255) {
+		value = 0;
+	}
+};
+var saveBtn = $('button#saveBtn');
+saveBtn.on('click', function(){
+	saveCanvas(canvas, 'myCanvas', 'jpg')
+});
+app.makeSound = function() {
 	$('div#sound').on('click',function() {
+		//if the sound is equal to a piece of audio
+		if (app.sound !== null) {
+			app.sound.pause();
+		}
 		// grab the sound
 		const sound = $(this).data('sound');
 		// use jQuery to grab the audio element with that class
 		const audio = $('.' + sound)[0];
+		app.sound = audio;
 		// restart it to zero
 		audio.currentTime = 0;
 		// stop it
-		audio.pause();
 		// play it
 		audio.play();
 		// add the animation  - a separate functions
@@ -42,7 +62,16 @@ app.init = function() {
 				// this runs once the animation is finished
 				element.removeClass('animated wobble');
 		});
-	}
+	};
+	// $('select#colors').change(function () {
+	// 	clrmode = $(this).val();
+	// 	// $('select#colors option:selected').each(function() {
+	// 	// 	clrmode = $(this).text();
+	// 	// });
+	// }).change();
+};
+app.init = function() {
+	app.makeSound();
 }
 $(function(){
 	app.init();
